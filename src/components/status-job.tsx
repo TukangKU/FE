3e978c2fe6dc4d94797e210bd91dbb15d9fd5c8d
Syafiq <syafiqsteam744@gmail.com>
2024-01-02 16:50:23 +0700

    fix; status payment

--- src/components/status-job.tsx
+++ src/components/status-job.tsx
@@ -22,7 +22,6 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Loader2 } from "lucide-react";
 
-
 interface Props {
   data: string;
 }
@@ -35,8 +34,7 @@ const StatusJob = (props: Props) => {
   const [job, setJob] = useState<JobWorker>();
   const params = useParams();
   const navigate = useNavigate();
-  const savedTransactionId = localStorage.getItem('transactionId');
-  console.log("data Payment",savedTransactionId);
+  const savedTransactionId = localStorage.getItem("transactionId");
 
   useEffect(() => {
     fetchData();
@@ -59,17 +57,13 @@ const StatusJob = (props: Props) => {
 
   const getStatusPayment = async () => {
     try {
-      const result = await getTransaction(params.id as string);
+      const result = await getTransaction(savedTransactionId as string);
 
       setStatusPayment(result);
       console.log(`data payment`, result);
       console.log(result);
     } catch (error: any) {
-      toast({
-        title: "Oops! Something went wrong.",
-        description: error.toString(),
-        variant: "destructive",
-      });
+      console.log(error);
     }
   };
 
@@ -177,16 +171,15 @@ const StatusJob = (props: Props) => {
           } ${data === "rejected" && "bg-red-600"} ${
             data === "negotiation_to_client" && "bg-slate-500"
           } ${data === "negotiation_to_worker" && "bg-slate-500"}
-           ${
-             statusPayment?.status === "Success" && "bg-blue-500"
-           } py-2 rounded-lg`}
+            py-2 rounded-lg`}
         >
           {role === "client" && (
             <>
-              {statusPayment?.status === "Pending" && data === "finished" ? (
+              {data === "finished" ? (
                 <Button
                   onClick={handleAcceptJob}
-                  className="w-full lg:text-3xl md:text-2xl text-xl font-bold h-16 bg-green-600 hover:bg-green-500"
+                  disabled={statusPayment?.status === "Success"}
+                  className={`w-full lg:text-3xl md:text-2xl text-xl font-bold h-16 bg-green-600 hover:bg-green-500`}
                 >
                   BAYAR
                 </Button>
